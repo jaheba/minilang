26ed38fa32296e03de3b432a8a9b39f3a84acc04
Jasper Schulz <jasper.b.schulz@gmail.com>
2016-01-29 15:47:25 +0000

    Updated grammar.

--- minilang/bytecode.py
+++ minilang/bytecode.py
@@ -5,15 +5,17 @@ from collections import namedtuple
 
 LabeledInstruction = namedtuple(
     'LabeledInstruction',
-    ['code', 'arg', 'lookup']
+    ['code', 'arg']
 )
 
+STATEMENTS = ['ASSIGN', 'COND', 'LOOP', 'RET', 'FUNC']
 
 class ByteCodeCompiler(object):
     def __init__(self):
         self.program = []
         self.labels = []
         self.labels_end = []
+        self.if_label = None
 
     def compile(self, ast):
         self.eval_block(ast)
@@ -23,7 +25,7 @@ class ByteCodeCompiler(object):
         for node in ast:
             bc_name = type(node).__name__
             self.eval(node)
-            if bc_name not in ['ASSIGN', 'LOOP']:
+            if bc_name not in STATEMENTS:
                 self.pop()
 
         return self.program
@@ -31,7 +33,8 @@ class ByteCodeCompiler(object):
     def _resolve_labels(self):
         for index, inst in enumerate(self.program):
             if type(inst) == LabeledInstruction:
-                arg = inst.lookup[inst.arg]
+                arg = self.labels_end[inst.arg]
+                # arg = inst.lookup[inst.arg]
 
                 self.program[index] = Instruction(
                     code=inst.code,
@@ -41,9 +44,9 @@ class ByteCodeCompiler(object):
     def pop(self):
         self.program.append(Instruction('POP', None))
 
-    def next_label(self):
+    def next_label(self, skip=0):
         index = len(self.labels)
-        self.labels.append(len(self.program))
+        self.labels.append(len(self.program) + skip)
         self.labels_end.append(None)
         return index
 
@@ -70,6 +73,35 @@ class ByteCodeCompiler(object):
     def LOAD_VAR(self, node):
         self.program.append(Instruction('LOAD_VAR', node.name))
 
+    def COND(self, node):
+        resolve_if = False
+        if not self.if_label:
+            resolve_if = True
+            self.if_label = self.next_label()
+
+        cond_label = self.next_label()
+
+        # eval condition
+        self.eval(node.condition)
+
+        # decide to jump or not
+        self.program.append(LabeledInstruction('JNE', cond_label))
+
+        # if no jump has happened, execute if body
+        self.eval_block(node.body)
+        self.program.append(LabeledInstruction('JUMP', self.if_label))
+
+        self.mark_label_end(cond_label)
+
+        self.eval_block(node.alt)
+
+        if resolve_if:
+            self.mark_label_end(self.if_label)
+            self.if_label = None
+
+
+
+
     def LOOP(self, node):
         label = self.next_label()
 
@@ -77,22 +109,28 @@ class ByteCodeCompiler(object):
         self.eval(node.condition)
 
         # decide to jump or not
-        self.program.append(LabeledInstruction('JNE', label, self.labels_end))
+        self.program.append(LabeledInstruction('JNE', label))
 
         # if no jump has happened, execute loop body
         self.eval_block(node.body)
+
         # jump back (body end)
         self.program.append(Instruction('JUMP', self.labels[label]))
 
         self.mark_label_end(label)
 
+    def COMP(self, node):
+        self._eval_left_right(node)
+        self.program.append(Instruction('COMP', node.type))
+
     def BINOP(self, node):
         self._eval_left_right(node)
 
-        if node.type == '<':
-            self.program.append(Instruction('COMP', 'LE'))
-        elif node.type == '+':
+        if node.type == '+':
             self.program.append(Instruction('PLUS', None))
+
+        elif node.type == '-':
+            self.program.append(Instruction('SUB', None))
         else:
             raise ValueError(node.type)
 
@@ -104,21 +142,48 @@ class ByteCodeCompiler(object):
         self.program.append(Instruction('CALL', len(node.args)))
 
 
-text = 'x:=0; while x < 5 { print(x); x := x+1; }'
+    def FUNC(self, node):
+        for arg in node.args[::-1]:
+            self.program.append(Instruction('ARG', arg))
+
+        self.program.append(Instruction('ARG_COUNT', len(node.args)))
+        self.program.append(Instruction('MAKE_FUNC', node.name))
+        
+        label = self.next_label(skip=1)
+        self.program.append(LabeledInstruction('JUMP', label))
+        
+        
+        self.eval_block(node.body)
+
+        if not self.program[-1].code == 'RETURN':
+            self.program.append(Instruction('RETURN', 0))
+
+        self.mark_label_end(label)
+
+    def RET(self, node):
+        self.eval(node.expression)
+        self.program.append(Instruction('RETURN', 1))
+
+    def PRINT(self, node):
+        self.eval(node.expression)
+        self.program.append(Instruction('PRINT', None))
+
+
+# text = 'x:=0; while x < 5 { print(x); x := x+1; }'
 
 from grammar import parse
-from interpreter import Intepreter, run
+# from interpreter import Intepreter, run
 
-from pprint import pprint
-# i = Intepreter()
+# from pprint import pprint
+# # i = Intepreter()
 
-ast = parse(text)
-# pprint(ast)
+# ast = parse(text)
+# # pprint(ast)
 
-bcc = ByteCodeCompiler()
+# bcc = ByteCodeCompiler()
 
-bcc.compile(ast)
+# bcc.compile(ast)
 
-i = run(bcc.program)
+# i = run(bcc.program)
 
-# print i.namespace
+# # print i.namespace
--- minilang/grammar/__init__.py
+++ minilang/grammar/__init__.py
@@ -4,7 +4,11 @@ Node = namedtuple('Node', ['type'])
 
 
 reserved = {
-    'while': 'WHILE'
+    'while': 'WHILE',
+    'if': 'IF',
+    'else': 'ELSE',
+    'fn': 'FN',
+    'print': 'PRINT',
 }
 
 tokens = [
@@ -12,14 +16,14 @@ tokens = [
     'COMP',
     'PLUS','MINUS','TIMES','DIVIDE',
     'LPAREN', 'RPAREN', 'LCURL', 'RCURL',
-    'SEMICOLON', 'COLON'
+    'SEMICOLON', 'COMMA', 'RETURN'
 ]
 
 tokens.extend(reserved.values())
 
 t_ASSIGN  = r':='
 # t_NUMBER  = r'\d+'
-t_COMP    = r'<'
+t_COMP    = r'==|!=|<|<=|>|>='
 
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
@@ -32,7 +36,9 @@ t_RPAREN  = r'\)'
 t_LCURL   = r'{'
 t_RCURL   = r'}'
 t_SEMICOLON = r';'
-t_COLON = r','
+t_COMMA = r','
+
+t_RETURN = r'\^'
 
 
 t_ignore = ' \t\n'
@@ -62,33 +68,41 @@ precedence = (
 )
 
 
-
-
 CONST_INT = namedtuple('CONST_INT', ['value'])
 BINOP = namedtuple('BINOP', ['type', 'left', 'right'])
+COMP = namedtuple('COMP', ['type', 'left', 'right'])
 ASSIGN = namedtuple('ASSIGN', ['name', 'value'])
 LOAD_VAR = namedtuple('LOAD_VAR', ['name'])
 LOOP = namedtuple('LOOP', ['condition', 'body'])
+COND = namedtuple('COND', ['condition', 'body', 'alt'])
 CALL = namedtuple('CALL', ['name', 'args'])
+FUNC = namedtuple('FUNC', ['name', 'args', 'body'])
+RET = namedtuple('RET', ['expression'])
+PRINT = namedtuple('PRINT', ['expression'])
 
 def p_statements(p):
-    '''statements : statement statement
+    '''statements : statements statement
                   | statement
     '''
 
-    p[0] = []
-    p[0].append(p[1])
-
-    if len(p) == 3:
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1]
         p[0].append(p[2])
 
 
 def p_statement(p):
     '''statement : assignment SEMICOLON
+                 | return SEMICOLON
                  | expression SEMICOLON
+                 | print SEMICOLON
+                 | if
                  | while
+                 | func
     '''
     p[0] = p[1]
+
 
 def p_assignment(p):
     '''assignment : NAME ASSIGN expression'''
@@ -99,14 +113,42 @@ def p_empty(p):
     pass
 
 def p_expression_list(p):
-    '''expression_list : expression COLON expression
+    '''expression_list : expression_list COMMA expression
                   | expression
                   | empty
     '''
 
-    p[0] = []
-    for e in p[1:]:
-        p[0].append(e)
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1]
+        p[0].append(p[2])
+
+def p_name_list(p):
+    '''name_list : NAME COMMA NAME
+            | NAME
+            | empty
+    '''
+
+    return p_expression_list(p)
+
+def p_func_def(p):
+    '''func : FN NAME name_list block'''
+
+    p[0] = FUNC(
+        name=p[2],
+        args=p[3],
+        body=p[4]
+        )
+
+def p_return(p):
+    '''return : RETURN expression'''
+    p[0] = RET(p[2])
+
+
+def p_print(p):
+    '''print : PRINT expression'''
+    p[0] = PRINT(p[2])
 
 def p_expression_call(p):
     '''expression : expression LPAREN expression_list RPAREN
@@ -118,11 +160,17 @@ def p_expression_binop(p):
                   | expression MINUS expression
                   | expression TIMES expression
                   | expression DIVIDE expression
-                  | expression COMP expression
     '''
 
     p[0] = BINOP(
         type=p[2],
+        left=p[1],
+        right=p[3],
+    )
+
+def p_expression_comp(p):
+    'expression : expression COMP expression'
+    p[0] = COMP(type=p[2],
         left=p[1],
         right=p[3],
     )
@@ -145,12 +193,32 @@ def p_while(p):
         condition=p[2],
         body=p[3])
 
+def p_if(p):
+    '''if : IF expression block ELSE block
+          | IF expression block ELSE if
+          | IF expression
+    '''
+    # else if
+    if len(p) == 6:
+        if type(p[5]) != list:
+            p[5] = [p[5]]
+        p[0] = COND(
+            condition=p[2],
+            body=p[3],
+            alt=p[5])
+    else:
+        p[0] = COND(
+            condition=p[2],
+            body=p[3],
+            alt=None)
+
 def p_block(p):
     'block : LCURL statements RCURL'
     p[0] = p[2]
 
 def p_error(t):
     print("Syntax error at '%s'" % t.value)
+    raise ValueError("Syntax error at '%s'" % t)
 
 from pprint import pprint
 import ply.yacc as yacc
