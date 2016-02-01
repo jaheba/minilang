ec031936ef983d861a11f97480ba6b268a6b37a7
Jasper Schulz <jasper.b.schulz@gmail.com>
2016-02-01 13:48:18 +0000

    Package settings.

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@ from setuptools import setup
 
 setup(
     name="minilang",
-    packages=['minilang'],
+    packages=['minilang', 'rpin'],
     version="0.0.1",
     author="Jasper Schulz",
     author_email="jasper.b.schulz@gmail.com",
-    install_requires=['ply'],
+    install_requires=['ply', 'click'],
 )
-
-
