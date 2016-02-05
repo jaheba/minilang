1b7e213c1ed10d4c02acadb4a62e8a6c577e4b7d
Jasper Schulz <jasper.b.schulz@gmail.com>
2016-02-05 11:43:55 +0100

    Added binary selection for benchmarks

--- benchmarks/bench.py
+++ benchmarks/bench.py
@@ -19,9 +19,8 @@ def compile_mm(path):
 def multitime(cmd, n=1):
     subprocess.call(['multitime', '-n', str(n)] + cmd)
 
-def bench_mm(target, n):
-    interpreter = os.path.normpath(os.path.join(basepath, '..', 'target-c'))
-    multitime([interpreter, os.path.abspath(target)], n=n)
+def bench_mm(target, n, bin):
+    multitime([bin, os.path.abspath(target)], n=n)
 
 def bench_pypy(target, n):
     multitime(['pypy', target], n=n)
@@ -33,7 +32,7 @@ def bench_cpython(target, n):
 COMPILING = click.style('Compiling: ', fg='yellow')
 BENCHING = click.style('Benchmarking: ', fg='cyan')
 
-def bench(name, n=1):
+def bench(name, n=1, bins=None, cpython=False):
     py_path = os.path.join(basepath, 'py', name + '.py')
     mm_path = os.path.join(basepath, 'mm', name + '.mm')
     mmc_path = mm_path + 'c'
@@ -42,29 +41,50 @@ def bench(name, n=1):
     compile_mm(mm_path)
 
     # mm
-    print
-    click.echo(BENCHING + name + ' (mm)')
-    bench_mm(mmc_path, n)
+    for bin in bins:
+        print
+        click.echo(BENCHING + os.path.basename(name) + ' (%s)' %os.path.basename(bin))
+
+        bench_mm(mmc_path, n, bin)
 
     # pypy
     print
     click.echo(BENCHING + name + '(PyPy)')
     bench_pypy(py_path, n)
 
-    # cpython
-    print
-    click.echo(BENCHING + name + '(CPython)')
-    bench_cpython(py_path, n)
+    if cpython:
+        print
+        click.echo(BENCHING + name + '(CPython)')
+        bench_cpython(py_path, n)
 
 @click.command()
 @click.option('-n', default=1)
+@click.option('--target', '-t', default='*')
+@click.option('--cpython', default=False)
 @click.argument('name')
-def cli(name, n):
-    if re.match('\d+', name):
+def cli(name, n, target, cpython):
+    binpath = os.path.normpath(os.path.join(basepath, '..', 'bin'))
+    bins = []
+
+    target_match = re.match(r'([\.0-9]+)\+', target)
+    if target_match:
+        target_ = map(int, target_match.groups()[0].split('.'))
+        for path in glob.glob(os.path.join(binpath, 'rpin*')):
+            match = re.match(r'rpin(\d+\.\d+\.\d+)', path.rsplit('/',1)[1])
+            if match:
+                other = map(int, match.groups(0)[0].split('.'))
+                if target_ <= other:
+                    bins.append(path)
+
+    # elif target=='*' or re.match('r[\.0-9]+', target):
+    #     bins = glob.glob(os.path.join(binpath, 'rpin.*'+target))
+
+
+    if re.match(r'\d+', name):
         try:
             path = glob.glob(os.path.join(basepath, 'mm', name + '-*.mm'))[0]
             b_name = os.path.basename(path).rsplit('.', 1)[0]
-            bench(b_name, n)
+            bench(b_name, n, bins, cpython=cpython)
         except IndexError:
             print 'No matching benchmark ' + name
 
